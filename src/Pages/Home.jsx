98bcb73cdf9d14211b7e31a6dda098d68557fbae
amantyagi22 <amantyagi2k@gmail.com>
2022-06-05 20:39:45 +0530

    Designed Front Page of the App

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUserName] = useState("");
+
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+  };
+
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLable">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+          />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have an invite create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Build With 💜 by &nbsp;
+          <a href="https://github.com/amantyagi22/amantyagi22">Aman Tyagi</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
