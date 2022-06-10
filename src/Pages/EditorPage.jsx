ea8efe6ad82647dc32d435358a2c149c5a998bf6
amantyagi22 <amantyagi2k@gmail.com>
2022-06-10 20:26:27 +0530

    Edit EditorPage

--- src/Pages/EditorPage.jsx
+++ src/Pages/EditorPage.jsx
@@ -1,13 +1,82 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Client from "../components/Client";
 import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import ACTIONS from "../Actions";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
+import toast from "react-hot-toast";
 
 const EditorPage = () => {
+  // Component not re-render and avail data during mulitple render
+  const socketRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+
+  const reactNavigator = useNavigate();
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          // socketRef.current.emit(ACTIONS.SYNC_CODE, {
+          //   code: codeRef.current,
+          //   socketId,
+          // });
+        }
+      );
+
+      // Listening for disconnected
+      // socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+      //   toast.success(`${username} left the room.`);
+      //   setClients((prev) => {
+      //     return prev.filter((client) => client.socketId !== socketId);
+      //   });
+      // });
+    };
+    init();
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    };
+  }, []);
+  // If we do not give empty array useEffect will render on every render
+
   const [clients, setClients] = useState([
     { socketId: 1, username: "Aman Tyagi" },
     { socketId: 2, username: "Rakesh" },
     { socketId: 3, username: "John Doe" },
   ]);
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div className="mainWrap">
